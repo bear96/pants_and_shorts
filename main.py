07273eade0251a7dd403e207f5aa1429e177df37
Aritra Majumdar <aritram21@vt.edu>
2023-07-18 11:43:51 -0400

    updated main

--- main.py
+++ main.py
@@ -17,12 +17,13 @@ df = df.T
 df.to_csv("data.csv")
 print("===========ALL DONE============")
 
-# for i in range(10):
-#     plt.plot(df[str(i)])
+df = pd.read_csv("data.csv")
+for i in range(10):
+    plt.plot(df[str(i)])
 
-# plt.grid()
-# plt.ylim(0,10)
-# plt.xlabel("Steps")
-# plt.ylabel("No of Workers")
-# plt.savefig("plot_of_pants_over_time.jpg")
-# plt.show()
+plt.grid()
+plt.ylim(0,10)
+plt.xlabel("Steps")
+plt.ylabel("No of Workers")
+plt.savefig("plot_of_pants_over_time.jpg")
+plt.show()
